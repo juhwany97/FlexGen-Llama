8dc3a9fe97848c7b709a43b85d424752d8a14bb1
juhwant97 <juhwan.cho@scale.snu.ac.kr>
2024-10-04 06:37:49 +0000

    update sep_layer config

--- flexgen/flex_opt.py
+++ flexgen/flex_opt.py
@@ -996,6 +996,51 @@ class TransformerLayer:
         self.attention.forward(hidden, cache_read_buf, read_buf1, attention_mask,
                                cache_write_buf, i, k)
         self.mlp.forward(hidden, None, read_buf2, attention_mask, None, i, k)
+        
+class TransformerLayer_llama:
+    def __init__(self, config, env, policy, i):
+        self.attention = LlamaGroupedQueryAttention(config, env, policy, i)
+        self.mlp = Llama3wayMLP(config, env, policy, i)
+        self.policy = policy
+        self.compute = self.attention.compute
+
+    def set_task(self, task):
+        self.attention.set_task(task)
+        self.mlp.set_task(task)
+
+    def init_weight(self, weight_home, path):
+        home1, home2 = ValueHolder(), ValueHolder()
+        self.attention.init_weight(home1, path)
+        self.mlp.init_weight(home2, path)
+        weight_home.store((home1, home2))
+
+    def load_weight(self, weight_home, weight_read_buf, k):
+        read_buf1, read_buf2 = ValueHolder(), ValueHolder()
+        home1, home2 = weight_home.val
+        self.attention.load_weight(home1, read_buf1, k)
+        self.mlp.load_weight(home2, read_buf2, k)
+        if k == 0:
+            weight_read_buf.store((read_buf1, read_buf2))
+
+    def init_cache_one_gpu_batch(self, cache_home):
+        self.attention.init_cache_one_gpu_batch(cache_home)
+
+    def load_cache(self, cache_home, cache_read_buf, i):
+        self.attention.load_cache(cache_home, cache_read_buf, i)
+
+    def store_cache(self, cache_home, cache_write_buf, i):
+        self.attention.store_cache(cache_home, cache_write_buf, i)
+
+    def forward(self, hidden, cache_read_buf, weight_read_buf, attention_mask,
+                cache_write_buf, i, k):
+        if k == self.policy.num_gpu_batches - 1:
+            read_buf1, read_buf2 = weight_read_buf.pop()
+        else:
+            read_buf1, read_buf2 = weight_read_buf.val
+
+        self.attention.forward(hidden, cache_read_buf, read_buf1, attention_mask,
+                               cache_write_buf, i, k)
+        self.mlp.forward(hidden, None, read_buf2, attention_mask, None, i, k)
 
 
 class OptLM:
@@ -1587,8 +1632,11 @@ class LlamaLM:
         layers = []
         layers.append(LlamaRotaryEmbedding(self.config, self.env, self.policy))
         for i in range(self.config.num_hidden_layers):
-            layers.append(LlamaGroupedQueryAttention(self.config, self.env, self.policy, i))
-            layers.append(Llama3wayMLP(self.config, self.env, self.policy, i))
+            if policy.sep_layer:
+                layers.append(LlamaGroupedQueryAttention(self.config, self.env, self.policy, i))
+                layers.append(Llama3wayMLP(self.config, self.env, self.policy, i))
+            else:
+                layers.append(TransformerLayer_llama(self.config, self.env, self.policy, i))
         layers.append(LlamaLMHead(self.config, self.env, self.policy))
         self.layers = layers
         self.num_layers = len(layers)
